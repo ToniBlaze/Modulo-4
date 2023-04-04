6a5a3f9df815410c70dd05023a844147f7573169
Alessio <alessio.toninello@gmail.com>
2023-04-04 08:15:04 +0200

    da verificare come fare carrello

--- D4/main.js
+++ D4/main.js
@@ -36,3 +36,40 @@ function insertImg(results) {
     elem.innerHTML = "Price: " + "<b>" + results[i].price + "€" + "</b>";
   });
 }
+
+function HideElement() {
+  const hideButtons = document.querySelectorAll(".hide");
+  hideButtons.forEach((elem) => {
+    elem.addEventListener("click", function (event) {
+      let cards = event.target.closest(".card");
+      cards.remove();
+    });
+  });
+}
+HideElement();
+
+function AddToCart() {
+  const AddToCartButtons = document.querySelectorAll(".add-to-cart");
+  AddToCartButtons.forEach((elem) => {
+    elem.addEventListener("click", function (event) {
+      let cards = event.target.closest(".card");
+      cards.classList.add("selected");
+
+      // //AGGIUNGI AL CARRELLO
+      // let image = cards.querySelector("img").src;
+      // let titolo = cards.querySelector(".card-title").innerText;
+      // let categorie = cards.querySelector(".book-category").innerText;
+      // let asinNr = cards.querySelector(".book-asin").innerText;
+      // let bookPrice = cards.querySelector(".book-price").innerText;
+
+      // const carrello = document.getElementById("cart");
+      // let col = document.createElement("div");
+      // col.classList.add("------");
+      // col.innerHTML = 
+
+      // AGGIUNGERE ELEMENTI AL CARRELLO
+      // NON FUNZIONA DA MODIFICARE
+    });
+  });
+}
+AddToCart();
